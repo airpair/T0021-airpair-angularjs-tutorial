dc2c87a63684c5e25897285f5c7f441a94490599
A K Srivastava <cs.abhilive@live.com>
2016-03-25 21:31:29 +0530

    Fixed Slider Issue on Home Page.

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@
  * Main AngularJS Web Application
  */
 var app = angular.module('tutorialWebApp', [
-  'ngRoute'
+  'ngRoute','ngAnimate', 'ui.bootstrap'
 ]);
 
 /**
@@ -30,6 +30,31 @@ app.config(['$routeProvider', function ($routeProvider) {
     .otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
 }]);
 
+/*use template cache to override carousel functionality - prev & next arrow glyphicon replaced by font-awesome icons.*/
+app.run(function($templateCache) {
+  $templateCache.put("uib/template/carousel/carousel.html",
+      "<div ng-mouseenter=\"pause()\" ng-mouseleave=\"play()\" class=\"carousel\" ng-swipe-right=\"prev()\" ng-swipe-left=\"next()\">\n" +
+      " <div class=\"carousel-inner\" ng-transclude></div>\n" +
+      " <a role=\"button\" href class=\"left carousel-control\" ng-click=\"prev()\" ng-show=\"slides.length > 1\">\n" +
+      " <span aria-hidden=\"true\" class=\"icon-prev\"></span>\n" +
+      " <span class=\"sr-only\">previous</span>\n" +
+      " </a>\n" +
+      " <a role=\"button\" href class=\"right carousel-control\" ng-click=\"next()\" ng-show=\"slides.length > 1\">\n" +
+      " <span aria-hidden=\"true\" class=\"icon-next\"></span>\n" +
+      " <span class=\"sr-only\">next</span>\n" +
+      " </a>\n" +
+      " <ol class=\"carousel-indicators\" ng-show=\"slides.length > 1\">\n" +
+      " <li ng-repeat=\"slide in slides | orderBy:indexOfSlide track by $index\" ng-class=\"{ active: isActive(slide) }\" ng-click=\"select(slide)\">\n" +
+      " <span class=\"sr-only\">slide {{ $index + 1 }} of {{ slides.length }}<span ng-if=\"isActive(slide)\">, currently active</span></span>\n" +
+      " </li>\n" +
+      " </ol>\n" +
+      "</div>\n" +
+    "");
+  $templateCache.put("uib/template/carousel/slide.html",
+      "<div ng-class=\"{'active': active}\" class=\"item text-center\" ng-transclude></div>\n" +
+    "");
+});
+
 /**
  * Controls the Blog
  */
@@ -40,16 +65,25 @@ app.controller('BlogCtrl', function (/* $scope, $location, $http */) {
 /**
  * Controls all other Pages
  */
-app.controller('PageCtrl', function (/* $scope, $location, $http */) {
+app.controller('PageCtrl', function ( $scope, $location, $http ) {
   console.log("Page Controller reporting for duty.");
 
+  $scope.slideInterval = 3000;
+	$scope.noWrapSlides = false;
+	$scope.active = 0;
+	  var slides = $scope.slides = [
+			{image: 'http://placehold.it/1900x455&text=Slide One', text: 'Caption 1', id: 0 },
+			{image: 'http://placehold.it/1900x455&text=Slide Two', text: 'Caption 2', id: 1 },
+			{image: 'http://placehold.it/1900x455&text=Slide Three', text: 'Caption 3', id: 2 }
+	  ];
+	  var currentIndex = 1;
   // Activates the Carousel
-  $('.carousel').carousel({
+  /*$('.carousel').carousel({
     interval: 5000
-  });
+  });*/
 
   // Activates Tooltips for Social Links
-  $('.tooltip-social').tooltip({
+  /*$('.tooltip-social').tooltip({
     selector: "a[data-toggle=tooltip]"
-  })
+  })*/
 });
